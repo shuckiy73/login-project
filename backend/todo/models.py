775e8401912c08c973f6e3db103f75de00cdb99e
shuckiy73 <shuckiy73@yandex.ru>
2024-12-19 19:49:06 +0300

    new file

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,136 +1,189 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-
 class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
+    name = models.CharField(unique=True, max_length=150, verbose_name="Название")
 
     class Meta:
-        managed = False
-        db_table = 'auth_group'
+        managed = True
+        db_table = 'todo_auth_group'  # Добавлен префикс todo_
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
+
+    def __str__(self):
+        return self.name
 
 
 class AuthGroupPermissions(models.Model):
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING, verbose_name="Группа")
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING, verbose_name="Разрешение")
 
     class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
+        managed = True
+        db_table = 'todo_auth_group_permissions'  # Добавлен префикс todo_
         unique_together = (('group', 'permission'),)
+        verbose_name = "Разрешение группы"
+        verbose_name_plural = "Разрешения групп"
+
+    def __str__(self):
+        return f"{self.group} - {self.permission}"
 
 
 class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
+    name = models.CharField(max_length=255, verbose_name="Название")
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, verbose_name="Тип контента")
+    codename = models.CharField(max_length=100, verbose_name="Кодовое имя")
 
     class Meta:
-        managed = False
-        db_table = 'auth_permission'
+        managed = True
+        db_table = 'todo_auth_permission'  # Добавлен префикс todo_
         unique_together = (('content_type', 'codename'),)
+        verbose_name = "Разрешение"
+        verbose_name_plural = "Разрешения"
+
+    def __str__(self):
+        return self.name
 
 
 class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    date_joined = models.DateTimeField()
+    password = models.CharField(max_length=128, verbose_name="Пароль")
+    last_login = models.DateTimeField(blank=True, null=True, verbose_name="Последний вход")
+    is_superuser = models.IntegerField(verbose_name="Суперпользователь")
+    username = models.CharField(unique=True, max_length=150, verbose_name="Имя пользователя")
+    first_name = models.CharField(max_length=30, verbose_name="Имя")
+    last_name = models.CharField(max_length=150, verbose_name="Фамилия")
+    email = models.CharField(max_length=254, verbose_name="Email")
+    is_staff = models.IntegerField(verbose_name="Персонал")
+    is_active = models.IntegerField(verbose_name="Активен")
+    date_joined = models.DateTimeField(verbose_name="Дата регистрации")
 
     class Meta:
-        managed = False
-        db_table = 'auth_user'
+        managed = True
+        db_table = 'todo_auth_user'  # Добавлен префикс todo_
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.username
 
 
 class AuthUserGroups(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING, verbose_name="Пользователь")
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING, verbose_name="Группа")
 
     class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
+        managed = True
+        db_table = 'todo_auth_user_groups'  # Добавлен префикс todo_
         unique_together = (('user', 'group'),)
+        verbose_name = "Группа пользователя"
+        verbose_name_plural = "Группы пользователей"
+
+    def __str__(self):
+        return f"{self.user} - {self.group}"
 
 
 class AuthUserUserPermissions(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING, verbose_name="Пользователь")
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING, verbose_name="Разрешение")
 
     class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
+        managed = True
+        db_table = 'todo_auth_user_user_permissions'  # Добавлен префикс todo_
         unique_together = (('user', 'permission'),)
+        verbose_name = "Разрешение пользователя"
+        verbose_name_plural = "Разрешения пользователей"
+
+    def __str__(self):
+        return f"{self.user} - {self.permission}"
 
 
 class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    action_time = models.DateTimeField(verbose_name="Время действия")
+    object_id = models.TextField(blank=True, null=True, verbose_name="ID объекта")
+    object_repr = models.CharField(max_length=200, verbose_name="Представление объекта")
+    action_flag = models.PositiveSmallIntegerField(verbose_name="Флаг действия")
+    change_message = models.TextField(verbose_name="Сообщение об изменении")
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True, verbose_name="Тип контента")
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING, verbose_name="Пользователь")
 
     class Meta:
-        managed = False
-        db_table = 'django_admin_log'
+        managed = True
+        db_table = 'todo_django_admin_log'  # Добавлен префикс todo_
+        verbose_name = "Журнал администрирования"
+        verbose_name_plural = "Журналы администрирования"
+
+    def __str__(self):
+        return f"{self.action_time} - {self.user}"
 
 
 class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
+    app_label = models.CharField(max_length=100, verbose_name="Метка приложения")
+    model = models.CharField(max_length=100, verbose_name="Модель")
 
     class Meta:
-        managed = False
-        db_table = 'django_content_type'
+        managed = True
+        db_table = 'todo_django_content_type'  # Добавлен префикс todo_
         unique_together = (('app_label', 'model'),)
+        verbose_name = "Тип контента"
+        verbose_name_plural = "Типы контента"
+
+    def __str__(self):
+        return f"{self.app_label} - {self.model}"
 
 
 class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
+    app = models.CharField(max_length=255, verbose_name="Приложение")
+    name = models.CharField(max_length=255, verbose_name="Название")
+    applied = models.DateTimeField(verbose_name="Применено")
 
     class Meta:
-        managed = False
-        db_table = 'django_migrations'
+        managed = True
+        db_table = 'todo_django_migrations'  # Добавлен префикс todo_
+        verbose_name = "Миграция"
+        verbose_name_plural = "Миграции"
+
+    def __str__(self):
+        return f"{self.app} - {self.name}"
 
 
 class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
+    session_key = models.CharField(primary_key=True, max_length=40, verbose_name="Ключ сессии")
+    session_data = models.TextField(verbose_name="Данные сессии")
+    expire_date = models.DateTimeField(verbose_name="Дата истечения")
 
     class Meta:
-        managed = False
-        db_table = 'django_session'
+        managed = True
+        db_table = 'todo_django_session'  # Добавлен префикс todo_
+        verbose_name = "Сессия"
+        verbose_name_plural = "Сессии"
+
+    def __str__(self):
+        return self.session_key
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=120)
-    description = models.TextField()
-    completed = models.IntegerField()
+    title = models.CharField(max_length=120, verbose_name="Заголовок")
+    description = models.TextField(verbose_name="Описание")
+    completed = models.IntegerField(verbose_name="Завершено")
 
     class Meta:
-        managed = False
-        db_table = 'todo_todo'
+        managed = True
+        db_table = 'todo_todo'  # Добавлен префикс todo_
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
+
+    def __str__(self):
+        return self.title
 
 
 class TodoUser(models.Model):
-    username = models.CharField(max_length=30, blank=True, null=True)
-    password = models.CharField(max_length=30, blank=True, null=True) 
+    username = models.CharField(max_length=30, blank=True, null=True, verbose_name="Имя пользователя")
+    password = models.CharField(max_length=30, blank=True, null=True, verbose_name="Пароль")
+
     class Meta:
-        managed = False
-        db_table = 'todo_user'
+        managed = True
+        db_table = 'todo_todo_user'  # Добавлен префикс todo_
+        verbose_name = "Пользователь задачи"
+        verbose_name_plural = "Пользователи задач"
+
+    def __str__(self):
+        return self.username if self.username else "Без имени"
--- backend/todo/serializers.py
+++ backend/todo/serializers.py
@@ -1,10 +1,14 @@
-
-# todo/serializers.py
-
 from rest_framework import serializers
 from .models import Todo
-      
+
 class TodoSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Todo
-    fields = ('id', 'title', 'description', 'completed')
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'description', 'completed')
+        # Добавляем verbose_name для полей
+        extra_kwargs = {
+            'id': {'label': 'ID'},
+            'title': {'label': 'Заголовок'},
+            'description': {'label': 'Описание'},
+            'completed': {'label': 'Завершено'},
+        }
